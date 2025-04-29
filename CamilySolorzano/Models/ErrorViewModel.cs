9964799fd8a857c1573883409b2752aa0853f270
laboratorio <laboratorio@UPOAULA10721>
2025-04-29 07:22:02 -0500

    Agregar archivos de proyecto.

---
+++ CamilySolorzano/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CamilySolorzano.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
